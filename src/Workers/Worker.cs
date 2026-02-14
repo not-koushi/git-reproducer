47190e72a2317efc449d5caec85878613b26e426
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-14 21:33:17 +0530

    feat: implement background job processor

--- src/Workers/Worker.cs
+++ src/Workers/Worker.cs
@@ -1,16 +1,36 @@
+using Application.Abstractions;
+using Domain;
+
 namespace Workers;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker:BackgroundService
 {
+    private readonly ILogger<Worker> _logger;
+    private readonly IJobQueue _queue;
+
+    public Worker(ILogger<Worker> logger, IJobQueue queue)
+    {
+        _logger = logger;
+        _queue = queue;
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Worker started");
+        
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (logger.IsEnabled(LogLevel.Information))
-            {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
+            var job = await _queue.DequeueAsync(stoppingToken);
+
+            job.Status = JobStatus.Cloning;
+            
+            _logger.LogInformation("Processing repo: {Repo}", job.RepositoryUrl);
+            
+            await Task.Delay(2000, stoppingToken);
+            
+            job.Status = JobStatus.Completed;
+            
+            _logger.LogInformation("Finished job: {Id}", job.Id);
         }
     }
 }
