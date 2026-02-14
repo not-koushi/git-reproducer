99847ebd4ce2c55a7b996df055fe32e07ba5473d
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-14 21:24:58 +0530

    feat: register job service in dependency injection

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,12 @@
 using Application.Abstractions;
 using Infrastructure.Queues;
+using Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IJobQueue, InMemoryJobQueue>();
+
+builder.Services.AddSingleton<JobService>();
 
 var app = builder.Build();
 
