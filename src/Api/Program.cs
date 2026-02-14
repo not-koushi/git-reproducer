aee212decdd115217705b58f680ae4b4b90364f4
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-14 21:49:10 +0530

    feat: configure sqlite job database

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,12 +2,17 @@ using Application.Abstractions;
 using Infrastructure.Queues;
 using Application.Services;
 using Application.Contracts;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IJobQueue, InMemoryJobQueue>();
 
 builder.Services.AddSingleton<JobService>();
+
+builder.Services.AddDbContext<JobDbContext>(options =>
+    options.UseSqlite("Data source = jobs.db"));
 
 var app = builder.Build();
 
