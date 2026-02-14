4e4834236b0ab6327a00711bf99a6472429d55eb
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-14 21:38:14 +0530

    configure dependency injection and queue

--- src/Workers/Program.cs
+++ src/Workers/Program.cs
@@ -1,6 +1,10 @@
+using Application.Abstractions;
+using Infrastructure.Queues;
 using Workers;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<IJobQueue, InMemoryJobQueue>();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
