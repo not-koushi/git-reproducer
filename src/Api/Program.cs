1cf9048c75c39f1686f3529384e9635253e58f7a
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-14 21:28:06 +0530

    feat: add endpoint to create reproduction jobs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,7 @@
 using Application.Abstractions;
 using Infrastructure.Queues;
 using Application.Services;
+using Application.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,5 +12,11 @@ builder.Services.AddSingleton<JobService>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Git Reproducer API running");
+
+app.MapPost("/jobs", (CreateJobRequest request, JobService service) =>
+{
+    var job = service.Create(request);
+    return Results.Ok(job);
+});
 
 app.Run();
