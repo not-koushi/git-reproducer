45b8dc9b78ea15e1a9250daa72bcc8d13f37d56d
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-02-16 18:52:18 +0530

    feat: add job retrieval contract

--- src/Application/Abstractions/IJobRepository.cs
+++ src/Application/Abstractions/IJobRepository.cs
@@ -5,4 +5,5 @@ namespace Application.Abstractions;
 public interface IJobRepository
 {
     Task<ReproductionJob> AddAsync(ReproductionJob job, CancellationToken ct = default);
+    Task<ReproductionJob?> GetAsync(Guid id, CancellationToken ct = default);
 }
